1ef68f199ea30be8c39d8b647fd1926b6054ace5
ibrahimd0 <1br4h1m.d0@gmail.com>
2023-06-16 18:12:17 -0700

    testing

---
+++ app.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, url_for, redirect, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    reps = db.Column(db.String(20), nullable=False, unique=True)
+    sets = db.Column(db.String(20), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    workouts = db.relationship('Workout')
+
+
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('homepage'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Username does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(username) < 4:
+            flash('username must be greater than 3 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(username=username, password=generate_password_hash(
+                password1, method='sha256'))
+
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+
+            flash('Account created!', category='success')
+            return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+@app.route('/homepage', methods=['GET', 'POST'])
+def homepage():
+    return render_template('homepage.html')
+@app.route('/workout', methods=['GET', 'POST'])
+def work_out():
+    if request.method == "POST":
+        name = request.method.get['name']
+        reps = request.method.get['reps']
+        sets = request.method.get['sets']
+
+        workouts = Workout(name=name, reps=reps, sets=sets, user_id = current_user)
+        db.session.add(workouts)
+        db.session.commit()
+    return render_template('workout.html',user=current_user)
+
+@app.route('/progress', methods=['GET', 'POST'])
+def display_workouts():
+    workouts = Workout.query.filter_by(user_id=current_user.id).all()
+    return render_template('progress.html', workouts=workouts)
+if __name__ == "__main__":
+    app.run(debug=True)
