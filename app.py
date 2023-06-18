2803fe48d7d24910dd3edf745a276f46061d1b64
ibrahimd0 <1br4h1m.d0@gmail.com>
2023-06-18 00:50:51 -0700

    created table for workout, excersie and updated html files

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 app = Flask(__name__)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
@@ -21,19 +19,27 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class Workout(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    reps = db.Column(db.String(20), nullable=False)
-    sets = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    workouts = db.relationship('Workout')
+    workouts = db.relationship('Workout', backref='user')
 
 
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    exercises = db.relationship('Exercise', backref='workout', cascade='all, delete-orphan')
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    reps = db.Column(db.String(20), nullable=False)
+    sets = db.Column(db.String(20), nullable=False)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'))
 
 
 with app.app_context():
@@ -61,8 +67,6 @@ def login():
     return render_template("login.html", user=current_user)
 
 
-
-
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
@@ -79,9 +83,9 @@ def signup():
 
         user = User.query.filter_by(username=username).first()
         if user:
-            flash('Email already exists.', category='error')
+            flash('Username already exists.', category='error')
         elif len(username) < 4:
-            flash('username must be greater than 3 characters.', category='error')
+            flash('Username must be greater than 3 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
@@ -92,32 +96,48 @@ def signup():
 
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
 
             flash('Account created!', category='success')
             return redirect(url_for('login'))
 
     return render_template('signup.html')
 
+
 @app.route('/homepage', methods=['GET', 'POST'])
 def homepage():
     return render_template('homepage.html')
+
+
 @app.route('/workout', methods=['GET', 'POST'])
+@login_required
 def work_out():
     if request.method == "POST":
-        name = request.form.get('name')
-        reps = request.form.get('reps')
-        sets = request.form.get('sets')
+        workoutNames = request.form.getlist('workoutName')
+        exerciseNames = request.form.getlist('name[]')
+        reps = request.form.getlist('reps[]')
+        sets = request.form.getlist('sets[]')
 
-        workouts = Workout(name=name, reps=reps, sets=sets, user_id = current_user.id)
-        db.session.add(workouts)
-        db.session.commit()
-    return render_template('workout.html',user=current_user)
+        for i in range(len(workoutNames)):
+            workout = Workout(name=workoutNames[i], user_id=current_user.id)
+            db.session.add(workout)
+            db.session.commit()
+
+            for j in range(len(exerciseNames)):
+                exercise = Exercise(name=exerciseNames[j], reps=reps[j], sets=sets[j], workout_id=workout.id)
+                db.session.add(exercise)
+                db.session.commit()
+
+        flash('Workouts saved successfully!', category='success')
+
+    return render_template('workout.html', user=current_user)
+
+
 
 @app.route('/progress', methods=['GET', 'POST'])
 def display_workouts():
-
-
     return render_template('progress.html', user=current_user)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
